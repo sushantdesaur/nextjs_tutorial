88b5c7c8e2569c9a4efd0a2bf576c52bb8c1278a
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-16 14:07:56 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          All right reserved
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
