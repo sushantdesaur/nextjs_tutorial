797de22d5e9f36ce5161f4b4d6f332dc2cc2a659
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-20 22:36:10 +0530

    Added Card to the about page

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,31 @@
 import MainNav from '../components/nav/MainNav.js'
-import { Container, Row, Col } from 'react-bootstrap';
+import Image from "next/image";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const About = () => {
   return (
     <>
-      <MainNav/>
-      <Container>
+      <MainNav />
+      <Container fluid className="d-flex justify-content-center mt-4">
         <Row>
-          <Col>About Page</Col>
+          <Col>
+            <Card style={{ height: "30rem", width: "50rem" }}>
+             <Card.Img as={ <Image
+                src="/David_Beckham.jpg"
+                alt="About Image"
+                width={200}
+                height={400}
+              />}/>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </>
